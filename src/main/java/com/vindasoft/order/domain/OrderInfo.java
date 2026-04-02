fc8ec63ef3b87437f96358deded139f1e6790dc1
jwd <jwd_open@126.com>
2026-04-02 22:05:08 +0800

    重新提交框架代码

---
+++ src/main/java/com/vindasoft/order/domain/OrderInfo.java
@@ -0,0 +1,25 @@
+/**
+ * @copyright vindasoft in 2026-01-01 version V1.0
+ */
+
+package com.vindasoft.order.domain;
+
+import lombok.Data;
+
+/**
+ * @Description: 订单信息
+ * @author: jwd
+ * @date: 2026-01-02
+ */
+@Data
+public class OrderInfo {
+    private String orderId;
+    private String userId;
+    private String productId;
+    private String status;
+    private String createTime;
+    private String updateTime;
+    private String deleteTime;
+    private String remark;
+    private String version;
+}
